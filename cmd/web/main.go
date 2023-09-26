b6156e7ced321df66f1b82e9255dd28e926b9dc5
Chul-Woong Yang <cwyang@gmail.com>
2023-09-26 02:31:01 +0000

    Template & HTML/CSS UI

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,9 @@ func main() {
 	mux.HandleFunc("/note", showNote) // note: `/note/` means `/foo/*`
 	mux.HandleFunc("/note/create", createNote)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("server started on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
