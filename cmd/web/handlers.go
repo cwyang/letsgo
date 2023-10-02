0c6e8ed4988725ad586565abad5551fad8693da7
Chul-Woong Yang <cwyang@gmail.com>
2023-10-02 16:42:54 +0000

    pat router

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,10 +10,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 	n, err := app.notes.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -26,7 +22,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showNote(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -46,14 +42,10 @@ func (app *application) showNote(w http.ResponseWriter, r *http.Request) {
 		Note: n,
 	})
 }
+func (app *application) createNoteForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new note!"))
+}
 func (app *application) createNote(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(405)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
 	title := "Quick Brown Fox"
 	content := "Jumps over the\nLittle Lazy Dog!"
 	expires := "7"
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,20 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"	// router
 	"github.com/justinas/alice"	// for chaining middleware
 )
 
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	
-	mux := http.NewServeMux()
-	// longer URL patterns are matched
-	mux.HandleFunc("/", app.home)         // catch all
-	mux.HandleFunc("/note", app.showNote) // note: `/note/` means `/foo/*`
-	mux.HandleFunc("/note/create", app.createNote)
+	mux := pat.New()
+
+	// Pat matches pattern in the registration order
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/note/create", http.HandlerFunc(app.createNoteForm))
+	mux.Post("/note/create", http.HandlerFunc(app.createNote))
+	mux.Get("/note/:id", http.HandlerFunc(app.showNote))
+
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return standardMiddleware.Then(mux)
 }
