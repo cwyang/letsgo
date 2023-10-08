3a25fa8f15b96287c5c5172fafee2948a5b3c34a
Chul-Woong Yang <cwyang@gmail.com>
2023-10-08 18:03:51 +0000

    api: /v1/healthcheck

---
+++ furthur/cmd/api/healthcheck.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// handler is method to create dependency
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
