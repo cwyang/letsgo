0b2b8c74a64194678d0ee4f480845eef4ede97cd
Chul-Woong Yang <cwyang@gmail.com>
2023-10-09 02:30:35 +0000

    json response

--- furthur/cmd/api/healthcheck.go
+++ furthur/cmd/api/healthcheck.go
@@ -6,8 +6,24 @@ import (
 )
 
 // handler is method to create dependency
+func (app *application) healthcheckHandler_v1(w http.ResponseWriter, r *http.Request) {
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
+}
+
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+	}
 }
