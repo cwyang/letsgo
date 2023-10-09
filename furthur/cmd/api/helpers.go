c38ddfc33bd48bc729aedf6cb11442a9fa296971
Chul-Woong Yang <cwyang@gmail.com>
2023-10-09 02:58:28 +0000

    json formatting and enveloping

--- furthur/cmd/api/helpers.go
+++ furthur/cmd/api/helpers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]interface{}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -19,8 +21,11 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 }
 
 func (app *application) writeJSON(w http.ResponseWriter,
-	status int, data interface{}, headers http.Header) error {
-	js, err := json.Marshal(data)
+	status int, data envelope, headers http.Header) error {
+
+	// no line prefix(""), tab indents("\t")
+	js, err := json.MarshalIndent(data, "", "\t")
+
 	if err != nil {
 		return err
 	}
