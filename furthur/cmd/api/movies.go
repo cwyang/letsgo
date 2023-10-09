c4f7f737a3328330eaf0ce49fb83d363a94c09f1
Chul-Woong Yang <cwyang@gmail.com>
2023-10-09 12:43:21 +0000

    Sending Error Messages

--- furthur/cmd/api/movies.go
+++ furthur/cmd/api/movies.go
@@ -31,7 +31,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "Error", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
