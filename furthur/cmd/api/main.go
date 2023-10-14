af6630e4885bbc712410b421f2cb22eabecec69c
Chul-Woong Yang <cwyang@gmail.com>
2023-10-14 17:09:40 +0000

    PostgreSQL connection

--- furthur/cmd/api/main.go
+++ furthur/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -8,8 +10,10 @@ import (
 	_ "net/http"
 	"os"
 	"time"
+
 	// "os"
 	// "time"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -17,6 +21,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -29,9 +36,24 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+
+	dsn := os.Getenv("GREENLIGHT_DB_DSN")
+	if dsn == "" {
+		dsn = "postgres://greenlight:password@localhost/greenlight"
+	}
+	flag.StringVar(&cfg.db.dsn, "db-dsn", dsn, "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("db connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -47,6 +69,22 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
---
+++ furthur/db/setup.sql
@@ -0,0 +1,8 @@
+create database greenlight;
+create extension if not exists citext; -- case insenstive string type
+
+-- create role greenlight with login password 'password';
+
+
+
+
