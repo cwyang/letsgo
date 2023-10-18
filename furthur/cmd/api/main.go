abcc7a254cf771ab82a1d72b0ebc6e3fac075df6
Chul-Woong Yang <cwyang@gmail.com>
2023-10-18 09:32:23 +0000

    CRUD /v1/movies

--- furthur/cmd/api/main.go
+++ furthur/cmd/api/main.go
@@ -13,6 +13,7 @@ import (
 
 	// "os"
 	// "time"
+	"github.com/cwyang/letsgo/furthur/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -32,6 +33,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -65,6 +67,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
