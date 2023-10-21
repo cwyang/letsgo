b2f77fb29d208425caba91bfdf79f0a75af01585
Chul-Woong Yang <cwyang@gmail.com>
2023-10-21 13:46:33 +0000

    api: PUT /v1/users/activated

--- furthur/internal/data/users.go
+++ furthur/internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"time"
@@ -171,4 +172,46 @@ returning version`
 		}
 	}
 	return nil
+}
+
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+
+	query := `
+select u.id, u.created_at, u.name, u.email, u.password_hash, u.activated, u.version
+from users as u
+inner join tokens
+on u.id = tokens.user_id
+where tokens.hash = $1
+and tokens.scope = $2
+and tokens.expiry > $3`
+
+	// use slice since passing an array is not supported by pq driver.
+	args := []interface{}{tokenHash[:], tokenScope, time.Now()}
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
