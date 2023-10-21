5a71fbb3e38ec73d90feacc91f7609b34206c61a
Chul-Woong Yang <cwyang@gmail.com>
2023-10-21 00:47:47 +0000

    json log

--- furthur/cmd/api/main.go
+++ furthur/cmd/api/main.go
@@ -14,6 +14,7 @@ import (
 	// "os"
 	// "time"
 	"github.com/cwyang/letsgo/furthur/internal/data"
+	"github.com/cwyang/letsgo/furthur/internal/jsonlog"
 	_ "github.com/lib/pq"
 )
 
@@ -32,7 +33,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -54,15 +55,15 @@ func main() {
 
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
 
-	logger.Printf("db connection pool established")
+	logger.PrintInfo("db connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -76,11 +77,15 @@ func main() {
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		ErrorLog:     log.New(logger, "" /* no prefix */, 0 /* no flag */),
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
