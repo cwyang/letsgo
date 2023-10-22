c51a9823debfadc05124b260ffcbfcc953f9033d
Chul-Woong Yang <cwyang@gmail.com>
2023-10-22 04:16:41 +0000

    some endpoints require authentication

--- furthur/cmd/api/errors.go
+++ furthur/cmd/api/errors.go
@@ -60,8 +60,19 @@ func (app *application) invalidCredentialResponse(w http.ResponseWriter, r *http
 	message := "invalid credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
 func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("WWW-Authenticate", "Bearer")
 	message := "invalid auth token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must be authenticated first"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your account must be activated first"
+	app.errorResponse(w, r, http.StatusForbidden, message)
 }
